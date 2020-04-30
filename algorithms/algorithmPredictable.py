62a4905a06f1f9c5da2d19d566fbfe704f310be5
Bohdan Yevtushenko <evtushencko.b@gmail.com>
2020-04-30 16:45:58 +0300

    #2. Refactor a little bit.

--- algorithms/algorithmPredictable.py
+++ algorithms/algorithmPredictable.py
@@ -1,9 +1,3 @@
-def getAnotherPlayer(player):
-    if (player == 'o'):
-        return 'x'
-    else:
-        return 'o'
-
 def algorithm(matrix, height, width, player, winCount):
     i = 0
     while(matrix[i][winCount - 1] != '-1'):
